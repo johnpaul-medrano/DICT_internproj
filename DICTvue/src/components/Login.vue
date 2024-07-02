0decaf22f0fef349ad37f63d4d8bd1d222e2a53a
danlor <21-06678@g.batstate-u.edu.ph>
2024-07-02 15:55:28 +0800

    Push #2
    added: icon, Project_page,

--- DICTvue/src/components/Login.vue
+++ DICTvue/src/components/Login.vue
@@ -6,10 +6,10 @@
       <div class="Login-box">
       <form @submit.prevent="handleLogin">
         <div class="form-group">
-          <input type="text" id="username" v-model="username" placeholder="     Username" required />
+          <input type="text" id="username" v-model="username" placeholder="Username" required />
         </div>
         <div class="form-group">
-          <input type="password" id="password" v-model="password" placeholder="    Password" required />
+          <input type="password" id="password" v-model="password" placeholder="Password" required />
         </div>
         <button type="submit">LOGIN</button>
       </form>
@@ -33,10 +33,9 @@ export default {
   },
   methods: {
     handleLogin() {
-      // Basic login logic (you can replace this with actual authentication logic)
       if (this.username === 'admin' && this.password === 'password') {
         alert('Login successful!');
-        this.$router.push('/home');
+        this.$router.push('/Projects');
       } else {
         alert('Invalid username or password');
       }
@@ -45,97 +44,4 @@ export default {
 };
 </script>
 
-<style>
-h1{
-  font-size: 40px;
-  color: #ffff;
-}
-body{
-  font-family: 'Poppins', sans-serif;
-  background-color: #275598;
-}
-
-.login-page {
-  display: flex;
-  height: 100vh;
-  background-color: #275598;
-
-}
-
-.login-container {
-  background-color: #275598;
-  flex: 1;
-  display: flex;
-  flex-direction: column;
-  justify-content: center ;
-  align-items: center;
-  text-align: center;
-  padding: 20px;
-}
-
-.form-group {
-  margin-bottom: 15px;
-  width: 100%;
-  text-align: center;
-}
-
-label {
-  display: block;
-  margin-bottom: 5px;
-}
-
-input {
-  border-radius: 20px;
-  height: 40px;
-  width: 300px;
-  padding: 8px; 
-  background-color:#d9d9d9;
-  margin-top: 10px;
-  font-size: 20px;
-  font-weight: 700;
-  outline: none;
-  border: none;
-}
-button {
-  height: 45px;
-  margin-top: 20px;
-  padding: 10px;
-  border-radius: 25px;
-  width: 60%;
-  background-color: #FBD35A;
-  color: #4961A6;
-  border: none;
-  cursor: pointer;
-  font-weight: 800;
-  font-size: 15px;
-}
-
-button:hover {
-  background-color: #ff5757;
-}
-
-.logo-container {
-  background-color: #275598;
-  flex: 1;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-.Login-box{
-  width: 450px;
-  height: 350px;
-  background-color: #ffff;
-  border-radius: 15px;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  align-self: center;
-}
-
-.logo {
-  max-width: 100%;
-  max-height: 100%;
-}
-</style>
+<style src="./Login.css"></style>
--- DICTvue/src/router/index.js
+++ DICTvue/src/router/index.js
@@ -2,11 +2,13 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Login from '../components/Login.vue';
 import Home from '../components/Home.vue';
+import Projects from '../components/Projects.vue';
 
 const routes = [
   { path: '/', redirect: '/login' },
   { path: '/login', component: Login },
   { path: '/home', component: Home },
+  { path: '/Projects', component: Projects},
 ];
 
 const router = createRouter({
