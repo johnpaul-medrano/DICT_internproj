2c6a30b72396f742b472e9aad12418338ad1fed7
danlor <21-06678@g.batstate-u.edu.ph>
2024-07-10 15:25:02 +0800

    css changes AHHAAH
    Sorry po

--- src/components/Ulo.vue
+++ src/components/Ulo.vue
@@ -1,13 +1,16 @@
 <template>
-          <header class="header">
-        <div class="header-content">
-          <div class="title-logo-container">
-            <h1 class="title"></h1>
-            <div class="settings-icon">
+  <header class="header">
+<div class="header-content">
+  <div class="title-logo-container">
+    <h1 class="title">DICT DOCTRACKER</h1>
+    <h1 class="title"></h1>
+    <div class="settings-icon">
+      <img src="@/assets/logo.png" alt="Settings Icon"/>
 
-            </div>
-          </div>
-        </div>
-      </header>
+    </div>
+  </div>
+</div>
+</header>
 </template>
+
 <style scoped src="./Ulo.css"></style>
