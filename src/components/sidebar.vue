19df5ef2cc8ce7d253139d7d80db25b0725ab909
danlor <21-06678@g.batstate-u.edu.ph>
2024-07-15 15:17:26 +0800

    Restriction per roles
    added png, edited sidebar and router

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -10,87 +10,62 @@
     </div>
     <div class="bottom">
       <ul>
-        <li
-          @click="toggleSubMenu"
-          :class="{ active: isActive('/mainpage/' + currentLogo + '/prform') }"
-        >
+        <div class="menu">
+          <router-link v-if="canAccess('For Approval')" :to="`/mainpage/${currentLogo}/monitoring`" class="lol">
+            <li :class="{ active: isActive('/mainpage/' + currentLogo + '/monitoring') }">
+              <img :src="icon6" alt="icon" />
+              For Approval
+            </li>
+          </router-link>
+        </div>
+        <!-- Purchase Request, visible to ILCDB -->
+        <li v-if="canAccess('Purchase Request')" @click="toggleSubMenu" :class="{ active: isActive('/mainpage/' + currentLogo + '/prform') }">
           <img :src="icon1" alt="icon" />
           Purchase Request
         </li>
-        <ul v-if="isSubMenuVisible" class="sub-menu">
-          <router-link
-            :to="`/mainpage/${currentLogo}/prformsvp`"
-            class="sub-menu-link"
-          >
-            <li
-              :class="{
-                active: isActive('/mainpage/' + currentLogo + '/prformsvp'),
-              }"
-            >
+        <ul v-if="isSubMenuVisible && canAccess('Purchase Request')" class="sub-menu">
+          <router-link :to="`/mainpage/${currentLogo}/prformsvp`" class="sub-menu-link">
+            <li :class="{ active: isActive('/mainpage/' + currentLogo + '/prformsvp') }">
               Small Value Procurement
             </li>
           </router-link>
-          <router-link
-            :to="`/mainpage/${currentLogo}/prformfb`"
-            class="sub-menu-link"
-          >
-            <li
-              :class="{
-                active: isActive('/mainpage/' + currentLogo + '/prformfb'),
-              }"
-            >
+          <router-link :to="`/mainpage/${currentLogo}/prformfb`" class="sub-menu-link">
+            <li :class="{ active: isActive('/mainpage/' + currentLogo + '/prformfb') }">
               For Bidding
             </li>
           </router-link>
         </ul>
+        <!-- Monitoring, visible to all roles -->
         <div class="menu">
-          <router-link :to="`/mainpage/${currentLogo}/monitoring`" class="lol">
-            <li
-              :class="{
-                active: isActive('/mainpage/' + currentLogo + '/monitoring'),
-              }"
-            >
+          <router-link v-if="canAccess('Monitoring')" :to="`/mainpage/${currentLogo}/monitoring`" class="lol">
+            <li :class="{ active: isActive('/mainpage/' + currentLogo + '/monitoring') }">
               <img :src="icon2" alt="icon" />
               Monitoring
             </li>
           </router-link>
         </div>
+        <!-- Suppliers Database, visible to ILCDB -->
         <div class="menu">
-          <router-link
-            :to="`/mainpage/${currentLogo}/suppliers-database`"
-            class="lol"
-          >
-            <li
-              :class="{
-                active: isActive(
-                  '/mainpage/' + currentLogo + '/suppliers-database'
-                ),
-              }"
-            >
+          <router-link v-if="canAccess('Database')" :to="`/mainpage/${currentLogo}/suppliers-database`" class="lol">
+            <li :class="{ active: isActive('/mainpage/' + currentLogo + '/suppliers-database') }">
               <img :src="icon3" alt="icon" />
               Suppliers Database
             </li>
           </router-link>
         </div>
+        <!-- Templates, visible to ILCDB -->
         <div class="menu">
-          <router-link :to="`/mainpage/${currentLogo}/templates`" class="lol">
-            <li
-              :class="{
-                active: isActive('/mainpage/' + currentLogo + '/templates'),
-              }"
-            >
+          <router-link v-if="canAccess('Templates')" :to="`/mainpage/${currentLogo}/templates`" class="lol">
+            <li :class="{ active: isActive('/mainpage/' + currentLogo + '/templates') }">
               <img :src="icon4" alt="icon" />
               Templates
             </li>
           </router-link>
         </div>
+        <!-- Request, visible to ILCDB -->
         <div class="menu">
-          <router-link :to="`/mainpage/${currentLogo}/request`" class="lol">
-            <li
-              :class="{
-                active: isActive('/mainpage/' + currentLogo + '/request'),
-              }"
-            >
+          <router-link v-if="canAccess('Request')" :to="`/mainpage/${currentLogo}/request`" class="lol">
+            <li :class="{ active: isActive('/mainpage/' + currentLogo + '/request') }">
               <img :src="icon5" alt="icon" />
               Request
             </li>
@@ -109,6 +84,7 @@ import icon2 from "@/assets/monitoring 1.png";
 import icon3 from "@/assets/database 1.png";
 import icon4 from "@/assets/templates 1.png";
 import icon5 from "@/assets/application 1.png";
+import icon6 from "@/assets/approval.png";
 
 export default {
   data() {
@@ -119,20 +95,27 @@ export default {
       icon3,
       icon4,
       icon5,
+      icon6,
       isSubMenuVisible: false,
     };
   },
   computed: {
     ...mapGetters(["currentLogo"]),
-  },
-  watch: { 
-    $route(to) {
-      if (!to.path.startsWith(`/mainpage/${this.currentLogo}/prform`)) {
-        this.isSubMenuVisible = false;
-      }
-    },
+    userRole() {
+      return localStorage.getItem("userRole");
+    }
   },
   methods: {
+    canAccess(feature) {
+      const roleFeatures = {
+        "ILCDB": ["Purchase Request", "Monitoring", "Database", "Templates", "Request", ],
+        "TOD Head": ["For Approval","Monitoring"],
+        "Budget Division": ["For Approval","Monitoring"],
+        "RD": ["For Approval","Monitoring"],
+        "Supply Office": ["Monitoring"]
+      };
+      return roleFeatures[this.userRole].includes(feature);
+    },
     toggleSubMenu() {
       this.isSubMenuVisible = !this.isSubMenuVisible;
     },
@@ -141,6 +124,13 @@ export default {
     },
     navigateToProjects() {
       this.$router.push({ name: "Projects" });
+    },
+  },
+  watch: { 
+    $route(to) {
+      if (!to.path.startsWith(`/mainpage/${this.currentLogo}/prform`)) {
+        this.isSubMenuVisible = false;
+      }
     },
   },
 };
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,70 @@
 import { createRouter, createWebHistory } from "vue-router";
-
+import { createStore } from 'vuex';
+import { db, storage } from "@/firebaseConfig";
+import { collection, onSnapshot } from "firebase/firestore";
+import { ref, listAll, getDownloadURL } from "firebase/storage";
+import workforceLogo from '@/assets/workforcey.png';
+import sparkLogo from '@/assets/sparky.png';
+import tech4edLogo from '@/assets/tech4edy.png';
 import Login from "../pages/login.vue"; // Adjust the path relative to router/index.js
 import { getCurrentUser } from "@/firebaseConfig";
+
+// Vuex Store
+const store = createStore({
+  state: {
+    logo: {
+      workforce: workforceLogo,
+      spark: sparkLogo,
+      tech4ed: tech4edLogo,
+    },
+    currentLogo: '',
+    tableData: [],
+  },
+  mutations: {
+    setLogo(state, logo) {
+      state.logo = logo;
+    },
+    SET_TABLE_DATA(state, payload) {
+      state.tableData = payload;
+    },
+  },
+  actions: {
+    updateLogo({ commit }, logo) {
+      commit('setLogo', logo);
+    },
+    async fetchInitialTableData({ commit }) {
+      try {
+        const listRef = ref(storage, 'purchase_requests/');
+        const res = await listAll(listRef);
+        const files = await Promise.all(res.items.map(async (itemRef) => {
+          const url = await getDownloadURL(itemRef);
+          return { prnum: itemRef.name, downloadURL: url };
+        }));
+        console.log('Fetched initial data:', files);
+        commit('SET_TABLE_DATA', files);
+      } catch (error) {
+        console.error('Error fetching initial data:', error);
+      }
+    },
+    listenToTableData({ commit }) {
+      const purchaseRequestsRef = collection(db, "purchase_requests");
+      onSnapshot(purchaseRequestsRef, (snapshot) => {
+        const data = snapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
+        console.log('Updated data:', data);
+        commit('SET_TABLE_DATA', data);
+      }, (error) => {
+        console.error('Error listening to data:', error);
+      });
+    },
+  },
+  getters: {
+    currentLogo: (state) => state.logo,
+    tableData: (state) => state.tableData,
+  },
+});
 
 // Vue Router
 const routes = [
@@ -88,4 +151,4 @@ router.beforeEach(async (to) => {
   }
 });
 
-export { router };
+export { router, store };
