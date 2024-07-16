7e9e41f8818b2b1e55dd41b14d566e31052905ec
2002Yuri <corenandrei.andino@gmail.com>
2024-07-16 13:22:26 +0800

    logout inside mainpage + logout confirmation

--- src/components/Ulo.vue
+++ src/components/Ulo.vue
@@ -1,13 +1,52 @@
 <template>
   <header class="header">
-    <div class="header-content">
-      <div class="title-logo-container">
-        <h1 class="title">ILCDB</h1>
-        <div class="settings-icon">
-          <img src="@/assets/logo.png" alt="Settings Icon" />
-        </div>
-      </div>
+    <div id="invidiv"></div>
+    <div class="title-logo-container">
+      <h1 class="title">ILCDB</h1>
+      <img src="@/assets/logo.png" alt="Settings Icon" />
     </div>
+
+    <button id="signout" @click="showModal = true">
+      <Icon icon="material-symbols:logout" width="20" /> Sign-out
+    </button>
+
+    <SignOutModal
+      :isVisible="showModal"
+      @close="showModal = false"
+      @confirm="signOut"
+    />
   </header>
 </template>
+
+<script>
+import { ref } from "vue";
+import { Icon } from "@iconify/vue";
+import { useRouter } from "vue-router";
+import { logout } from "@/firebaseConfig";
+import SignOutModal from "@/components/signoutconfirm.vue";
+
+export default {
+  components: {
+    Icon,
+    SignOutModal,
+  },
+
+  setup() {
+    const router = useRouter();
+    const showModal = ref(false);
+
+    async function signOut() {
+      await logout();
+      router.push("/");
+      showModal.value = false;
+    }
+
+    return {
+      showModal,
+      signOut,
+    };
+  },
+};
+</script>
+
 <style scoped src="./Ulo.css"></style>
---
+++ src/components/signoutconfirm.vue
@@ -0,0 +1,100 @@
+<template>
+  <div v-if="isVisible" class="modal">
+    <div class="modal-content">
+      <span class="close" @click="closeModal">&times;</span>
+      <p>Are you sure you want to sign out?</p>
+      <button class="confirm-button" @click="confirmSignOut">Yes</button>
+      <button class="cancel-button" @click="closeModal">No</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isVisible: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  methods: {
+    closeModal() {
+      this.$emit("close");
+    },
+    confirmSignOut() {
+      this.$emit("confirm");
+    },
+  },
+};
+</script>
+
+<style scoped>
+.modal {
+  display: flex;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.4);
+  justify-content: center;
+  align-items: center;
+}
+
+.modal-content {
+  background-color: white;
+  padding: 20px;
+  border: 1px solid #888;
+  width: 400px;
+  height: 160px;
+  text-align: center;
+  border-radius: 20px;
+}
+
+.close {
+  color: #aaa;
+  float: right;
+  font-size: 28px;
+  font-weight: bold;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.confirm-button,
+.cancel-button {
+  background-color: #007bff;
+  color: white;
+  border: none;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 4px;
+  margin: 10px;
+}
+
+.confirm-button:hover,
+.cancel-button:hover {
+  background-color: #0056b3;
+}
+
+.cancel-button {
+  background-color: #6c757d;
+}
+
+.cancel-button:hover {
+  background-color: #5a6268;
+}
+
+p {
+  color: black;
+  margin-top: 40px;
+  margin-bottom: 30px;
+}
+</style>
